f4673d1f86de65f188e9e93064d7fb83a6d43a49
Vasu Chalasani <sc83@illinois.edu>
2021-04-21 23:11:52 -0500

    Add CLI args

--- apps/audio_processor_main.cc
+++ apps/audio_processor_main.cc
@@ -1,12 +1,16 @@
-//
-// Created by Vasu Chalasani on 4/21/21.
-//
 
 #include <iostream>
-#include <ostream>
 
-int main()
+int main(int argc, char **argv)
 {
+    std::cout << argc;
     return 0;
 }
+
+//TODO: : is used for inheritance, two params: number of args, args themselves, ** pointer to a string, represent an array as pointer to data type
+//binary is compiled file/program, inside cmkae build debugm use ./app name to run it 
+//need to rebuild changes before running on terminal (play on GUI is build and run), builds whats selected 
+//argv: first =  name of file
+//argc: arg count
+
 
