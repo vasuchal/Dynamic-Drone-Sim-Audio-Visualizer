5bf0ef545967d210452aac928dbbd7b37bf76c6e
Vasu Chalasani <sc83@illinois.edu>
2021-04-21 23:14:11 -0500

    Implement audio processing

--- include/core/audio_processor.h
+++ include/core/audio_processor.h
@@ -1,15 +1,19 @@
 #pragma once
-
+#include <string>
+#include "cinder/audio/audio.h"
+#include "audio_processor.h"
 
 namespace audio {
-
     class AudioProcessor{
     public:
+        AudioProcessor(std::string audio_file_path);
         AudioProcessor();
         void Display();
         void AdvanceOneFrame();
-
     private:
+        cinder::audio::MonitorSpectralNodeRef spectral_;
+        int bin_count_;
+        std::vector<float> magnitudes_of_freq_;
     };
 
 }  // namespace app
--- src/core/audio_processor.cc
+++ src/core/audio_processor.cc
@@ -1,16 +1,32 @@
+#include <cinder/audio/Context.h>
+#include "cinder/audio/audio.h"
 #include "../../include/core/audio_processor.h"
 
 namespace audio {
 
-    AudioProcessor::AudioProcessor() {
-
+    using namespace cinder::audio;
+    AudioProcessor::AudioProcessor(std::string file_path) {
+        bin_count_ = 0;
+        audio::SourceFileRef sourceFile = audio::load(cinder::loadFile(file_path ) );
+        audio::VoiceRef audio_output = audio::Voice::create(sourceFile);
+        auto ctx = audio::Context::master();
+        auto monitor_format = audio::MonitorSpectralNode::Format().fftSize(1024).windowSize(512);
+        spectral_ = ctx->makeNode( new audio::MonitorSpectralNode(monitor_format));
+        audio_output-> getOutputNode() >> spectral_;
+        ctx->enable();
+        audio_output->start();
+        spectral_->setSmoothingFactor(1);
     }
 
-    void AudioProcessor::Display() {
+    AudioProcessor::AudioProcessor() {};
 
+    void AudioProcessor::Display() {
+        
     }
 
     void AudioProcessor::AdvanceOneFrame() {
+        magnitudes_of_freq_ = spectral_->getMagSpectrum();
+        bin_count_ = spectral_->getNumBins();
 
     }
 }
