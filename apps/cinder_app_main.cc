bef6183228a18bd1bc79111280b6d980a3bab2dd
Vasu Chalasani <sc83@illinois.edu>
2021-04-21 20:30:10 -0500

    Connect visualizer to app main

--- apps/cinder_app_main.cc
+++ apps/cinder_app_main.cc
@@ -1,4 +1,11 @@
-//
-// Created by Vasu Chalasani on 4/21/21.
-//
+#include <visualizer/audio_visualizer.h>
 
+using audio::AudioVisualizerApp;
+
+void prepareSettings(AudioVisualizerApp::Settings* settings) {
+    settings->setResizable(false);
+    //TODO: edit settings
+}
+
+// This line is a macro that expands into an "int main()" function.
+CINDER_APP(AudioVisualizerApp, ci::app::RendererGl, prepareSettings);
