6cd0eced4aacaaba319a0fab7f75ef0b352e7e49
Vasu Chalasani <sc83@illinois.edu>
2021-05-03 23:02:47 -0500

    Implement extra 2d visualizer

---
+++ include/visualizer/2d_visualizer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+namespace audio {
+    class Visualizer2D{
+    public:
+        //TODO: add documentation
+        Visualizer2D();
+
+        /**
+          * Works with draw function and will display visual components relating to the magnitude of freqs of audio at a given slice
+          */
+        void Draw(std::vector<float> magnitudes_of_freq) const;
+    private:
+    };
+
+}
